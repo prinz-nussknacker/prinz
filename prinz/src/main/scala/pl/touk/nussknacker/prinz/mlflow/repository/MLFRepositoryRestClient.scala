a0e1d1de7fdce666abc1e5b70aa7c79bc674cbd9
Maciej Procyk <macieekprocyk@gmail.com>
2020-11-15 14:48:47 +0100

    add typed params to get requests

--- prinz/src/main/scala/pl/touk/nussknacker/prinz/mlflow/repository/MLFRepositoryRestClient.scala
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/mlflow/repository/MLFRepositoryRestClient.scala
@@ -5,7 +5,7 @@ import java.net.URL
 import pl.touk.nussknacker.prinz.mlflow.model.RegisteredModel
 import pl.touk.nussknacker.prinz.mlflow.MlflowConstants.{BASE_API_PATH, BASE_PREVIEW_API_PATH}
 import pl.touk.nussknacker.prinz.model.repository.ModelRepository
-import pl.touk.nussknacker.prinz.util.http.{RestClientException, RestJsonClient}
+import pl.touk.nussknacker.prinz.util.http.{RequestParams, RestClientException, RestJsonClient}
 
 
 class MLFRepositoryRestClient(hostUrl: URL) extends ModelRepository {
@@ -19,23 +19,22 @@ class MLFRepositoryRestClient(hostUrl: URL) extends ModelRepository {
     previewRestClient.getJson[GetAllRegisteredModelsResponse]("/registered-models/list").right.map(_.registered_models)
 
   def listModels(maxResults: Int, page: Int): RestResponse[List[RegisteredModel]] =
-    previewRestClient.getJsonBody[GetAllRegisteredModelsResponse]("/registered-models/list",
-      listModelsBody(maxResults, page)).right.map(_.registered_models)
+    previewRestClient.getJson[GetAllRegisteredModelsRequest, GetAllRegisteredModelsResponse](
+      "/registered-models/list", GetAllRegisteredModelsRequest(maxResults, page))
+      .right.map(_.registered_models)
 
   override def getModel(name: String): RestResponse[RegisteredModel] =
-    previewRestClient.getJsonBody[GetRegisteredModelResponse]("/registered-models/get",
-      getModelBody(name)).right.map(_.registered_model)
-
-  private def getModelBody(name: String) = s"""{"name": "${name}"}"""
-
-  private def listModelsBody(maxResults: Int, page: Int) = s"""{"max_results": ${maxResults}, "page_token": ${page}}"""
+    previewRestClient.getJson[GetRegisteredModelRequest, GetRegisteredModelResponse](
+      "/registered-models/get", GetRegisteredModelRequest(name))
+      .right.map(_.registered_model)
 }
 
 object MLFRepositoryRestClient {
   def apply(hostUrl: URL): MLFRepositoryRestClient = new MLFRepositoryRestClient(hostUrl)
 }
 
-//RESPONSES
-private case class GetAllRegisteredModelsResponse(registered_models: List[RegisteredModel])
+case class GetAllRegisteredModelsRequest(max_results: Int, page_token: Int) extends RequestParams[GetAllRegisteredModelsRequest]
+case class GetAllRegisteredModelsResponse(registered_models: List[RegisteredModel])
 
-private case class GetRegisteredModelResponse(registered_model: RegisteredModel)
+case class GetRegisteredModelRequest(name: String) extends RequestParams[GetRegisteredModelRequest]
+case class GetRegisteredModelResponse(registered_model: RegisteredModel)
--- prinz/src/main/scala/pl/touk/nussknacker/prinz/util/http/RestJsonClient.scala
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/util/http/RestJsonClient.scala
@@ -1,21 +1,21 @@
 package pl.touk.nussknacker.prinz.util.http
 
-import org.json4s.{DefaultFormats, Formats, JNothing, Serialization}
 import org.json4s.native.Serialization
-import sttp.client3.{Empty, HttpURLConnectionBackend, Identity, RequestT, ResponseException, SttpBackend, SttpClientException, UriContext, basicRequest}
+import org.json4s.{DefaultFormats, Formats, Serialization}
 import sttp.client3.json4s.SttpJson4sApi
-import sttp.model.Uri
+import sttp.client3.{HttpURLConnectionBackend, Identity, ResponseException, SttpBackend, SttpClientException, UriContext, basicRequest}
+import sttp.model.{Methods, Uri}
 
 //TODO: Parse object to JSON in body?
-class RestJsonClient(val baseUrl: String, private val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()) extends SttpJson4sApi {
+class RestJsonClient(val baseUrl: String, private val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()) extends SttpJson4sApi with Methods {
 
   private implicit val SERIALIZATION: Serialization = Serialization
   private implicit val FORMATS: Formats = DefaultFormats
 
-  def postJsonBody[RESPONSE: Manifest](relativePath: String, body: String): Either[RestClientException, RESPONSE] = {
+  def postJsonBody[BODY <: AnyRef, RESPONSE: Manifest]
+  (relativePath: String, body: BODY): Either[RestClientException, RESPONSE] = {
     val request = basicRequest
-      .post(uriFromRelativePath(relativePath))
-      .contentType("application/json")
+      .post(uriFromRelativePath(relativePath, EmptyRequestParams.getParamsMap))
       .body(body)
       .response(asJson[RESPONSE])
     wrapCaughtException(() => request.send(backend)
@@ -23,27 +23,20 @@ class RestJsonClient(val baseUrl: String, private val backend: SttpBackend[Ident
     )
   }
 
-  def getJson[RESPONSE: Manifest](relativePath: String): Either[RestClientException, RESPONSE] = {
+  def getJson[RESPONSE: Manifest](relativePath: String): Either[RestClientException, RESPONSE] =
+    getJson[Any, RESPONSE](relativePath, EmptyRequestParams)
+
+  def getJson[PARAMS, RESPONSE: Manifest](relativePath: String, params: RequestParams[PARAMS]): Either[RestClientException, RESPONSE] = {
     val request = basicRequest
-      .get(uriFromRelativePath(relativePath))
+      .get(uriFromRelativePath(relativePath, params.getParamsMap))
       .response(asJson[RESPONSE])
     wrapCaughtException(() => request.send(backend)
       .body.left.map(clientExceptionFromResponse)
     )
   }
 
-  def getJsonBody[RESPONSE: Manifest](relativePath: String, body: String): Either[RestClientException, RESPONSE] = {
-    val request = basicRequest
-      .get(uriFromRelativePath(relativePath))
-      .contentType("application/json")
-      .body(body)
-      .response(asJson[RESPONSE])
-    wrapCaughtException(() => request.send(backend)
-      .body.left.map(clientExceptionFromResponse)
-    )
-  }
-
-  private def uriFromRelativePath(relativePath: String): Uri = uri"$baseUrl$relativePath"
+  private def uriFromRelativePath(relativePath: String, params: Map[String, String]): Uri =
+    uri"${s"$baseUrl$relativePath"}?$params"
 
   private def clientExceptionFromResponse(value: ResponseException[String, Exception]): RestClientException =
     RestClientException(value.toString)
