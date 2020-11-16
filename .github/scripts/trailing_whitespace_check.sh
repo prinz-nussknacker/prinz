06e710f057f74318c45512dc2f717b7542c2a2d0
Maciej Procyk <macieekprocyk@gmail.com>
2020-11-16 16:41:44 +0100

    add own version of trailing whitespace check

--- .github/scripts/trailing_whitespace_check.sh
+++ .github/scripts/trailing_whitespace_check.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
 
 set -e
-
-tw_lines=""
-
-for file in $(git ls-files | sed -e 's/^/.\//')
-do
-  lines=$(egrep -rnIH " +$" $file | cut -f-2 -d ":")
-  if [ ! -z "$lines" ]; then
-    tw_lines+=$([[ -z "$tw_lines" ]] && echo "$lines" || echo $'\n'"$lines")
-  fi
-done
+files="$(find . -type d \( -path ./.git \
+  -o -path ./docs/imgs \                             # copy this line to add another directory to exclude in checking
+  \) -prune -o -type f -exec egrep -l " +$" {} \;)"
 
 exit_code=0
-
-if [ ! -z "$tw_lines" ]; then
-  echo -e "\n***** Lines containing trailing whitespace *****\n"
-  echo -e "${tw_lines[@]}"
-  echo -e "\nFailed.\n"
+if [ ! -z "$files" ]; then
+  printf "Files containing trailing whitespace\n"
+  printf '%s\n' $files
   exit_code=1
 fi
 
