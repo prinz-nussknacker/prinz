31d7bde7b7bbe450a622c05e4853ceae71664303
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-11-28 11:17:50 +0100

    Move to using sbtwrapper

--- dev-environment/create_environment.sh
+++ dev-environment/create_environment.sh
@@ -2,7 +2,7 @@
 
 # Compile and package prinz if not packaged yet
 cd '..' &&
-sbt prinz/package prinz_sample/package &&
+./sbtwrapper prinz/package prinz_sample/package &&
 cd - &&
 cp ../prinz/target/scala-2.12/prinz_2.12-0.0.1-SNAPSHOT.jar nussknacker/opt/prinz/ &&
 cp ../prinz_sample/target/scala-2.12/prinz-sample_2.12-0.0.1-SNAPSHOT.jar nussknacker/opt/prinz-sample/ &&
