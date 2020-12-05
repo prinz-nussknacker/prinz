70117e152cca3cf9c6e2ce31d962791b2f3c67b5
Jan Kukowski <57066180+xvk9@users.noreply.github.com>
2020-12-05 21:59:57 +0100

    MlflowDataConverter + tests (#53)
    * MlflowDataConverter + tests
    
    * Style fixes

---
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/util/converter/mlflow/MlflowDataConverter.scala
@@ -0,0 +1,57 @@
+package pl.touk.nussknacker.prinz.util.converter.mlflow
+
+import io.circe.{Decoder, Encoder, Json}
+import io.circe.generic.auto.{exportDecoder, exportEncoder}
+import io.circe.jawn.{parse, decode}
+import io.circe.syntax.EncoderOps
+import pl.touk.nussknacker.prinz.util.collection.immutable.SetMultimap
+
+object MlflowDataConverter {
+
+  implicit private val encodeDecimalOrString: Encoder[Either[BigDecimal, String]] =
+    Encoder.instance(_.fold(_.asJson, _.asJson))
+
+  implicit private val decodeDecimalOrString: Decoder[Either[BigDecimal, String]] =
+    Decoder[BigDecimal].map(Left(_)).or(Decoder[String].map(Right(_)))
+
+  case class Dataframe(columns: List[String], data: List[List[Either[BigDecimal, String]]]) {
+
+    def toList: List[(String, Either[BigDecimal, String])] =
+      (for (i <- columns.indices; record <- data) yield (columns(i), record(i))).toList
+  }
+
+  def toMultimap(data: String): SetMultimap[String, Either[BigDecimal, String]] = {
+    val parsedData = parse(data).getOrElse(Json.Null).noSpaces
+    val dataframe = decode[Dataframe](parsedData)
+      .getOrElse(throw new IllegalArgumentException("Invalid data"))
+
+    if (!isDataframeConvertible(dataframe)) {
+      throw new IllegalArgumentException("Invalid data")
+    }
+
+    val list = dataframe.toList
+    SetMultimap(list)
+  }
+
+  def toJsonString(multimap: SetMultimap[String, Either[BigDecimal, String]]): String = {
+    if (!isMultimapConvertible(multimap)) {
+      throw new IllegalArgumentException("Invalid multimap")
+    }
+
+    val columns = multimap.keys.toList
+    val data = multimap
+      .values
+      .flatMap(_.zipWithIndex)
+      .groupBy{case (num, idx) => idx}
+      .map{case (k, v) => v.map(_._1).toList}
+      .toList
+
+    Dataframe(columns, data).asJson.toString()
+  }
+
+  private def isDataframeConvertible(dataframe: Dataframe): Boolean =
+    !dataframe.data.exists(record => record.length != dataframe.columns.length)
+
+  private def isMultimapConvertible(multimap: SetMultimap[String, Either[BigDecimal, String]]): Boolean =
+    multimap.values.map(_.size).toSet.size <= 1
+}
--- prinz/src/test/scala/pl/touk/nussknacker/prinz/converter/mlflow/MlflowDataConverterTest.scala
+++ prinz/src/test/scala/pl/touk/nussknacker/prinz/converter/mlflow/MlflowDataConverterTest.scala
@@ -2,29 +2,23 @@ package pl.touk.nussknacker.prinz.converter.mlflow
 
 import pl.touk.nussknacker.prinz.UnitTest
 import pl.touk.nussknacker.prinz.util.collection.immutable.SetMultimap
+import pl.touk.nussknacker.prinz.util.converter.mlflow.MlflowDataConverter
+import cats.implicits.catsSyntaxEitherId
 
 class MlflowDataConverterTest extends UnitTest {
 
   "MlflowDataConverter" should "convert empty data input" in {
+    val inputData = """{
+                      |    "columns": [],
+                      |    "data": []
+                      |}""".stripMargin
 
+    val expected = SetMultimap.empty[String, Either[BigDecimal, String]]
+
+    MlflowDataConverter.toMultimap(inputData) should equal (expected)
   }
 
-  it should "convert data in map format" in {
-    val inputData = """[
-                      |    {"a": 1,"b": 2,"c": 3},
-                      |    {"a": 4,"b": 5,"c": 6}
-                      |]""".stripMargin
-
-    val expected = SetMultimap[String, Int](
-      "a" -> 1, "a" -> 4,
-      "b" -> 2, "b" -> 5,
-      "c" -> 3, "c" -> 6,
-    )
-
-    // TODO call converted method and check if expected data is returned
-  }
-
-  it should "convert data in list format" in {
+  it should "convert data in split format" in {
     val inputData = """{
                       |    "columns": ["a", "b", "c"],
                       |    "data": [[1, 2, 3], [4, 5, 6]]
@@ -35,15 +29,23 @@ class MlflowDataConverterTest extends UnitTest {
       "b" -> 2, "b" -> 5,
       "c" -> 3, "c" -> 6,
     )
-    // TODO call converted method and check if expected data is returned
+
+    val decimals = for (column <- expected.keys; value <- expected.get(column).get)
+      yield (column, BigDecimal(value).asLeft[String])
+
+    val expectedWithDecimals = SetMultimap(decimals)
+
+    MlflowDataConverter.toMultimap(inputData) should equal (expectedWithDecimals)
   }
 
-  it should "throw IllegalArgumentException for null input" in {
-    val inputData: String = null
+  it should "throw IllegalArgumentException for wrong input" in {
+    val inputData = """{
+                      |    "columns": ["a", "b", "c"],
+                      |    "data": [[1, 2], [4, 5]]
+                      |}""".stripMargin
 
     assertThrows[IllegalArgumentException] {
-      // TODO call converter with null argument
-      throw new IllegalArgumentException("Make it thrown by converter on bad input")
+      MlflowDataConverter.toMultimap(inputData)
     }
   }
 }
