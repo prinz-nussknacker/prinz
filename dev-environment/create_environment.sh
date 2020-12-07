5d01c8303b6b7fa7c0541550829a9a3dcdee9f32
Krzysztof Antoniak <kantoniak@kantoniak.com>
2020-12-07 22:29:32 +0100

    Clean up environment starter

--- dev-environment/create_environment.sh
+++ dev-environment/create_environment.sh
@@ -1,18 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 scalaV="2.12"
 nussknackerV="0.2.2"
 
+sampleDeps=(
+    "../prinz_sample/target/pack/lib/nussknacker-flink-api_${scalaV}-${nussknackerV}.jar"
+    "../prinz_sample/target/pack/lib/nussknacker-flink-util_${scalaV}-${nussknackerV}.jar"
+    "../prinz_sample/target/pack/lib/nussknacker-model-flink-util_${scalaV}-${nussknackerV}.jar"
+)
+
 # Compile and package prinz if not packaged yet
 cd .. &&
-./sbtwrapper prinz/package prinz_sample/package prinz_sample/pack &&
+./sbtwrapper prinz/package prinz_sample/pack &&
 cd - &&
 mkdir -p nussknacker/opt/prinz/ &&
 cp "../prinz/target/scala-${scalaV}/prinz_${scalaV}-0.0.1-SNAPSHOT.jar" "nussknacker/opt/prinz/" &&
 cp "../prinz_sample/target/scala-${scalaV}/prinz-sample_${scalaV}-0.0.1-SNAPSHOT.jar" "nussknacker/opt/prinz-sample/" &&
-cp "../prinz_sample/target/pack/lib/nussknacker-flink-api_${scalaV}-${nussknackerV}.jar" "nussknacker/opt/prinz-sample/" &&
-cp "../prinz_sample/target/pack/lib/nussknacker-flink-util_${scalaV}-${nussknackerV}.jar" "nussknacker/opt/prinz-sample/" &&
-cp "../prinz_sample/target/pack/lib/nussknacker-model-flink-util_${scalaV}-${nussknackerV}.jar" "nussknacker/opt/prinz-sample/" &&
+cp "${sampleDeps[@]}" "nussknacker/opt/prinz-sample/" &&
 
 # Add -d flag to hide environment startup
 docker-compose up --build
