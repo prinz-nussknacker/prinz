1d9b6d4735c6eaf4d8fa57410acb8e8025bbc514
Maciej Procyk <macieekprocyk@gmail.com>
2020-12-13 16:26:47 +0100

    fix lint errors

--- dev-environment/clear_environment.sh
+++ dev-environment/clear_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker-compose -f docker-compose.yaml -f docker-compose-env.yaml kill &&
-    docker-compose -f docker-compose.yaml -f docker-compose-env.yaml rm -f -v &&
+docker-compose -f docker-compose.yaml -f docker-compose-env.yaml rm -f -v &&
 rm -rf ./data/minio/* &&
 rm -rf ./data/postgres/*
