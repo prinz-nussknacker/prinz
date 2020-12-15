008577f23b349f20d01c264fdc87d4b43df9a7f4
Jadw1 <jadw1_1@o2.pl>
2020-12-15 15:45:19 +0100

    new, not working dynamic config

--- prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/SampleConfigCreator.scala
+++ prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/SampleConfigCreator.scala
@@ -1,11 +1,14 @@
 package pl.touk.nussknacker.prinz.sample
 
+import java.net.URL
+
 import pl.touk.nussknacker.engine.api.Service
 import pl.touk.nussknacker.engine.api.process.{ProcessObjectDependencies, SinkFactory, SourceFactory, WithCategories}
 import pl.touk.nussknacker.engine.flink.util.sink.EmptySink
 import pl.touk.nussknacker.engine.flink.util.transformer.PeriodicSourceFactory
 import pl.touk.nussknacker.engine.util.process.EmptyProcessConfigCreator
 import pl.touk.nussknacker.prinz.enrichers.PrinzEnricher
+import pl.touk.nussknacker.prinz.mlflow.repository.MLFRepository
 
 class SampleConfigCreator extends EmptyProcessConfigCreator {
 
@@ -19,7 +22,15 @@ class SampleConfigCreator extends EmptyProcessConfigCreator {
     "empty" -> allCategories(SinkFactory.noParam(EmptySink))
   )
 
-  override def services(processObjectDependencies: ProcessObjectDependencies): Map[String, WithCategories[Service]] = Map(
-    "prinzEnricher" -> allCategories(new PrinzEnricher)
-  )
+  override def services(processObjectDependencies: ProcessObjectDependencies): Map[String, WithCategories[Service]] = {
+    val repo = MLFRepository(new URL("http://localhost:5000"))
+    val response = repo.listModels
+
+    if(response.isRight) {
+      response.right.get.map(m => (m.getName.toString, allCategories(new PrinzEnricher))).toMap
+    }
+    else {
+      Map()
+    }
+  }
 }
