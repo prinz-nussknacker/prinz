1a3398116d77775894ad6912435663c2da611e63
Jadw1 <jadw1_1@o2.pl>
2020-12-15 16:16:20 +0100

    less logging

--- prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/SampleConfigCreator.scala
+++ prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/SampleConfigCreator.scala
@@ -2,6 +2,7 @@ package pl.touk.nussknacker.prinz.sample
 
 import java.net.URL
 
+import com.typesafe.scalalogging.Logger
 import pl.touk.nussknacker.engine.api.Service
 import pl.touk.nussknacker.engine.api.process.{ProcessObjectDependencies, SinkFactory, SourceFactory, WithCategories}
 import pl.touk.nussknacker.engine.flink.util.sink.EmptySink
@@ -11,6 +12,8 @@ import pl.touk.nussknacker.prinz.enrichers.PrinzEnricher
 import pl.touk.nussknacker.prinz.mlflow.repository.MLFRepository
 
 class SampleConfigCreator extends EmptyProcessConfigCreator {
+
+  private val logger = Logger[this.type]
 
   protected def allCategories[T](obj: T): WithCategories[T] = WithCategories(obj, "FraudDetection", "Recommendations")
 
@@ -25,6 +28,7 @@ class SampleConfigCreator extends EmptyProcessConfigCreator {
   override def services(processObjectDependencies: ProcessObjectDependencies): Map[String, WithCategories[Service]] = {
     val repo = MLFRepository(new URL("http://localhost:5000"))
     val response = repo.listModels
+    logger.debug(response.toString)
 
     if(response.isRight) {
       response.right.get.map(m => (m.getName.toString, allCategories(new PrinzEnricher))).toMap
