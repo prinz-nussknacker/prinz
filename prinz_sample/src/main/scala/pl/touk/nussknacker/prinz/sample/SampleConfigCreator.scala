1f410691ba24a5fcc45b28ec1029cc8efd131157
Maciej Procyk <macieekprocyk@gmail.com>
2020-12-18 23:20:54 +0100

    downgrade circe version add logging sink

--- prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/SampleConfigCreator.scala
+++ prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/SampleConfigCreator.scala
@@ -20,7 +20,8 @@ class SampleConfigCreator extends EmptyProcessConfigCreator {
   )
 
   override def sinkFactories(processObjectDependencies: ProcessObjectDependencies): Map[String, WithCategories[SinkFactory]] = Map(
-    "empty" -> allCategories(SinkFactory.noParam(EmptySink))
+    "empty" -> allCategories(SinkFactory.noParam(EmptySink)),
+    "logging" -> allCategories(SinkFactory.noParam(LoggingSink))
   )
 
   override def services(processObjectDependencies: ProcessObjectDependencies): Map[String, WithCategories[Service]] = {
