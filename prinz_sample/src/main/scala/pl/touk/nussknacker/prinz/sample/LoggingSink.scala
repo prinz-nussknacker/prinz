abb5c068db307d1509c7d702da011b8686650b1e
avan1235 <macieekprocyk@gmail.com>
2021-01-15 00:13:30 +0100

    add logging sinks

---
+++ prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/LoggingSink.scala
@@ -0,0 +1,19 @@
+package pl.touk.nussknacker.prinz.sample
+
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.flink.streaming.api.functions.sink.SinkFunction
+import org.apache.flink.streaming.api.functions.sink.SinkFunction.Context
+import pl.touk.nussknacker.engine.flink.api.process.BasicFlinkSink
+
+case class LoggingSink(prefix: String) extends BasicFlinkSink with LazyLogging {
+
+  override def testDataOutput: Option[(Any) => String] =
+    Some(value => s"$prefix - $value")
+
+  override def toFlinkFunction: SinkFunction[Any] = new SinkFunction[Any] with Serializable {
+    override def invoke(value: Any, context: Context[_]): Unit = {
+      val loggedMessage = s"$prefix - $value"
+      logger.info(loggedMessage)
+    }
+  }
+}
