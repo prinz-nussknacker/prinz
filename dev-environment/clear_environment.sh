a4f3cd7fcaa9800677b1dbfcfd43e81e97af6ca5
Jadw1 <jadw1_1@o2.pl>
2021-03-05 00:48:30 +0100

    separating docker compose for pmml and mlflow

--- dev-environment/clear_environment.sh
+++ dev-environment/clear_environment.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-docker-compose --env-file ../.env -f docker-compose.yaml -f docker-compose-env.yaml stop
-docker-compose --env-file ../.env -f docker-compose.yaml -f docker-compose-env.yaml kill
-docker-compose --env-file ../.env -f docker-compose.yaml -f docker-compose-env.yaml rm -f -v
+COMP_FILES=""
+
+if [ $# -eq 0 ]
+then
+    COMP_FILES="-f docker-compose-mlflow.yaml -f docker-compose-pmml.yaml"
+else
+    for FRAMEWORK in $@
+    do
+        if [ $FRAMEWORK = "mlflow" ]
+        then
+            COMP_FILES+="-f docker-compose-mlflow.yaml "
+        elif [ $FRAMEWORK = "pmml" ]
+        then
+            COMP_FILES+="-f docker-compose-pmml.yaml"
+        fi
+    done
+fi
+
+docker-compose --env-file ../.env ${COMP_FILES} -f docker-compose-env.yaml stop
+docker-compose --env-file ../.env ${COMP_FILES} -f docker-compose-env.yaml kill
+docker-compose --env-file ../.env ${COMP_FILES} -f docker-compose-env.yaml rm -f -v
 docker network prune -f
 docker container prune -f
 docker volume prune -f
--- dev-environment/create_environment.sh
+++ dev-environment/create_environment.sh
@@ -2,21 +2,46 @@
 
 scalaV="2.12"
 
-# Compile and package prinz if not packaged yet
-cd .. &&
-./sbtwrapper prinz_sample/assembly &&
-cd - &&
-mkdir -p nussknacker/opt/prinz/ &&
-cp "../prinz_sample/target/scala-${scalaV}/prinz-sample-assembly-0.0.1-SNAPSHOT.jar" "./nussknacker/opt/prinz-sample/" &&
+COMP_FILES=""
+ENV_FILE="-f docker-compose-env.yaml"
+COMPILE=true
+
+if [ $# -eq 0 ]
+then
+    COMP_FILES="-f docker-compose-mlflow.yaml -f docker-compose-pmml.yaml"
+else
+    for ARG in $@
+    do
+        if [ $ARG = "-e" ]
+        then
+            ENV_FILE=""
+            COMPILE=false
+        elif [ $ARG = "mlflow" ]
+        then
+            COMP_FILES+="-f docker-compose-mlflow.yaml "
+        elif [ $ARG = "pmml" ]
+        then
+            COMP_FILES+="-f docker-compose-pmml.yaml "
+        fi
+    done
+fi
+
+if [ $COMPILE = true ]
+then
+    echo "COOOOOMPILE"
+    # Compile and package prinz if not packaged yet
+    cd .. &&
+    ./sbtwrapper prinz_sample/assembly &&
+    cd - &&
+    mkdir -p nussknacker/opt/prinz/ &&
+    cp "../prinz_sample/target/scala-${scalaV}/prinz-sample-assembly-0.0.1-SNAPSHOT.jar" "./nussknacker/opt/prinz-sample/"
+fi
 
 # Create external network to communication between nussknacker and mlflow proxy
 docker network create dev-bridge-net
 
 # Add -d flag to hide environment startup
-docker-compose --env-file ../.env -f docker-compose.yaml -f docker-compose-env.yaml kill
-docker-compose --env-file ../.env -f docker-compose.yaml -f docker-compose-env.yaml rm -f -v
-docker-compose --env-file ../.env -f docker-compose.yaml -f docker-compose-env.yaml build
-docker-compose --env-file ../.env \
-               -f docker-compose.yaml \
-               -f docker-compose-env.yaml \
-               up --always-recreate-deps
+docker-compose --env-file ../.env ${COMP_FILES} ${ENV_FILE} kill
+docker-compose --env-file ../.env ${COMP_FILES} ${ENV_FILE} rm -f -v
+docker-compose --env-file ../.env ${COMP_FILES} ${ENV_FILE} build
+docker-compose --env-file ../.env ${COMP_FILES} ${ENV_FILE} up --always-recreate-deps
