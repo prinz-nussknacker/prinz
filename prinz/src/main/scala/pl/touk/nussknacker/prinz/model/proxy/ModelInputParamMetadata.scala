9405ffce7b686f30d78cca75f0d2a08e75b35d0e
avan1235 <macieekprocyk@gmail.com>
2021-03-09 23:40:00 +0100

    lint errors fixes

--- prinz/src/main/scala/pl/touk/nussknacker/prinz/model/proxy/ModelInputParamMetadata.scala
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/model/proxy/ModelInputParamMetadata.scala
@@ -3,4 +3,3 @@ package pl.touk.nussknacker.prinz.model.proxy
 import pl.touk.nussknacker.prinz.model.{ModelMetadata, SignatureName}
 
 final case class ModelInputParamMetadata(paramName: SignatureName, modelMetadata: ModelMetadata)
-
