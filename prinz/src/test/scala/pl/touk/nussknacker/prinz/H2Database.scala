5d0ba811c2f9c828ba57eae607cc709e370cbda6
avan1235 <macieekprocyk@gmail.com>
2021-03-09 22:49:57 +0100

    add db to tests

--- prinz/src/test/scala/pl/touk/nussknacker/prinz/H2Database.scala
+++ prinz/src/test/scala/pl/touk/nussknacker/prinz/H2Database.scala
@@ -9,11 +9,11 @@ import scala.reflect.io.Directory
 
 trait H2Database extends AnyFlatSpecLike with BeforeAndAfterAll {
 
-  private val PARENT_DIR: String = "./test-database"
+  private val DATABASE_PARENT_DIR: String = "./h2-test-database"
 
   private val DATABASE_NAME: String = "prinz-test-db"
 
-  private val DATABASE_DIR: String = s"$PARENT_DIR/$DATABASE_NAME"
+  private val DATABASE_DIR: String = s"$DATABASE_PARENT_DIR/$DATABASE_NAME"
 
   private val DATABASE_URL: String = s"jdbc:h2:$DATABASE_DIR"
 
@@ -40,7 +40,7 @@ trait H2Database extends AnyFlatSpecLike with BeforeAndAfterAll {
   }
 
   override protected def afterAll(): Unit = {
-    new Directory(new File(PARENT_DIR)).deleteRecursively()
+    new Directory(new File(DATABASE_PARENT_DIR)).deleteRecursively()
   }
 
   override protected def beforeAll(): Unit = {
