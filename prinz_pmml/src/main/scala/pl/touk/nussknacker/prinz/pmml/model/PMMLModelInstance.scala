bf64ee8c6525f8686cc6eb27958c62eb4bc986a2
Jan Kukowski <57066180+xvk9@users.noreply.github.com>
2021-03-11 10:26:20 +0100

    Add PMML model signature (#117)
    * Add PMMLSignatureProvider and PMMLSignatureInterpreter
    
    * InputStream in PMMLModel + base PMMLModelInstance
    
    * fix newline

--- prinz_pmml/src/main/scala/pl/touk/nussknacker/prinz/pmml/model/PMMLModelInstance.scala
+++ prinz_pmml/src/main/scala/pl/touk/nussknacker/prinz/pmml/model/PMMLModelInstance.scala
@@ -1,3 +1,11 @@
 package pl.touk.nussknacker.prinz.pmml.model
 
-class PMMLModelInstance
+import org.jpmml.evaluator.Evaluator
+import pl.touk.nussknacker.prinz.model.ModelInstance
+import pl.touk.nussknacker.prinz.util.collection.immutable.VectorMultimap
+
+case class PMMLModelInstance(evaluator: Evaluator, override val model: PMMLModel)
+  extends ModelInstance(model, PMMLSignatureProvider) {
+
+  override def run(inputMap: VectorMultimap[String, AnyRef]): ModelRunResult = ???
+}
