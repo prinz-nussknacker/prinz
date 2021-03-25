f9afb23a53fc954f2bd57881d4afb0444772c969
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-03-25 19:41:18 +0100

    Extract toMutableMap in VectorMultimapAsRowset

--- prinz/src/main/scala/pl/touk/nussknacker/prinz/util/collection/immutable/VectorMultimapUtils.scala
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/util/collection/immutable/VectorMultimapUtils.scala
@@ -5,9 +5,12 @@ import scala.collection.mutable
 object VectorMultimapUtils {
 
   implicit class VectorMultimapAsRowset[K, V](val inputsMap: VectorMultimap[K, V]) {
+
+    private def toMutableMap[T](immutable: collection.Map[K, T]): mutable.Map[K, T] = mutable.Map() ++ immutable
+
     def forEachRow[T](f: Map[K, V] => T): IndexedSeq[T] = {
       val totalTuples = inputsMap.map(_._2.length).max
-      val iteratorsMap = mutable.Map() ++ inputsMap.mapVectors(_.iterator)
+      val iteratorsMap = toMutableMap(inputsMap.mapVectors(_.iterator))
 
       (1 to totalTuples) map (_ => {
         val row = iteratorsMap map {
