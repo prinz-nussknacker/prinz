fdf634a3388e494be8e274ff988d9af91f46656d
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-03-25 17:04:22 +0100

    Fix immutable map of iterators

--- prinz/src/main/scala/pl/touk/nussknacker/prinz/util/collection/immutable/VectorMultimapUtils.scala
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/util/collection/immutable/VectorMultimapUtils.scala
@@ -1,11 +1,13 @@
 package pl.touk.nussknacker.prinz.util.collection.immutable
 
+import scala.collection.mutable
+
 object VectorMultimapUtils {
 
   implicit class VectorMultimapAsRowset[K, V](val inputsMap: VectorMultimap[K, V]) {
     def forEachRow[T](f: Map[K, V] => T): IndexedSeq[T] = {
       val totalTuples = inputsMap.map(_._2.length).max
-      val iteratorsMap = inputsMap.mapVectors(_.iterator)
+      val iteratorsMap = mutable.Map() ++ inputsMap.mapVectors(_.iterator)
 
       (1 to totalTuples) map (_ => {
         val row = iteratorsMap map {
