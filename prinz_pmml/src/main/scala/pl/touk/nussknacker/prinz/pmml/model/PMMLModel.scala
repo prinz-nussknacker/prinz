4d70e8d2c5dc4527164e07246313a9c733ed57c1
avan1235 <macieekprocyk@gmail.com>
2021-03-28 23:17:14 +0200

    add work dir env from file

--- prinz_pmml/src/main/scala/pl/touk/nussknacker/prinz/pmml/model/PMMLModel.scala
+++ prinz_pmml/src/main/scala/pl/touk/nussknacker/prinz/pmml/model/PMMLModel.scala
@@ -7,9 +7,20 @@ import pl.touk.nussknacker.prinz.pmml.repository.PMMLModelPayload
 import java.io.{File, InputStream}
 
 final class PMMLModel(payload: PMMLModelPayload) extends Model {
-  val evaluatorBuilder = new LoadingModelEvaluatorBuilder().load(payload.inputStream)
-  val optionalModelName = Option(evaluatorBuilder.getModel().getModelName())
+
+  val evaluatorBuilder: LoadingModelEvaluatorBuilder = new LoadingModelEvaluatorBuilder().load(payload.inputStream)
+
+  val optionalModelName: Option[String] = Option(evaluatorBuilder.getModel.getModelName)
+
   val evaluator: Evaluator = evaluatorBuilder.build()
+
+  try {
+    evaluator.verify()
+  } catch {
+    case ex: PMMLException => throw ModelNotValidException(this, ex)
+  } finally {
+    payload.inputStream.close()
+  }
 
   override def getName: PMMLModelName = extractName
 
@@ -18,12 +29,6 @@ final class PMMLModel(payload: PMMLModelPayload) extends Model {
   override def toModelInstance: ModelInstance = new PMMLModelInstance(evaluator, this)
 
   private def extractName: PMMLModelName = PMMLModelName(optionalModelName.getOrElse(payload.name))
-  
-  try {
-    evaluator.verify()
-  } catch {
-    case ex: PMMLException => throw new ModelNotValidException(this, ex)
-  }
 }
 
 case class PMMLModelName(name: String) extends ModelName(name)
@@ -31,11 +36,6 @@ case class PMMLModelName(name: String) extends ModelName(name)
 case class PMMLModelVersion(version: String) extends ModelVersion
 
 object PMMLModel {
-  def apply(payload: PMMLModelPayload): PMMLModel = new PMMLModel(payload)
 
-  private def buildModelEvaluatorFromStream(inputStream: InputStream): ModelEvaluator[_] = {
-    val model = new LoadingModelEvaluatorBuilder().load(inputStream).build()
-    inputStream.close()
-    model
-  }
+  def apply(payload: PMMLModelPayload): PMMLModel = new PMMLModel(payload)
 }
