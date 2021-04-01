e5e079ff71dd720acb4488cb50c5791d9bd2bc59
avan1235 <macieekprocyk@gmail.com>
2021-04-02 00:04:51 +0200

    lint fixes

--- dev-environment/pmml-samples/scripts/train.sh
+++ dev-environment/pmml-samples/scripts/train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python models/fraud-detection/train.py "exports/fraud-detection-v0-1.pmml" &
+python models/fraud-detection/train.py "exports/pmml-fraud-detection-v0-1.pmml" &
 python models/sklearn-elasticnet-wine/train.py 0.42 0.5 "exports/pmml-wine-042-05-v0-1.pmml" &
 python models/sklearn-elasticnet-wine/train.py 0.84 0.5 "exports/pmml-wine-084-05-v0-2.pmml"
 
