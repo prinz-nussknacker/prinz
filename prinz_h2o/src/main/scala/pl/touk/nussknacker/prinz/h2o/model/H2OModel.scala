6a09813dca956489e0876435d0c1989b55d39167
xvk9 <kukowskijan@gmail.com>
2021-04-14 22:37:19 +0200

    Errox fix

--- prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
+++ prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
@@ -1,13 +1,12 @@
 package pl.touk.nussknacker.prinz.h2o.model
 
-import hex.genmodel.GenModel
-import hex.genmodel.algos.deeplearning.DeeplearningMojoModel
+import hex.genmodel.{GenModel, MojoModel}
 import hex.genmodel.easy.EasyPredictModelWrapper
 import pl.touk.nussknacker.prinz.model.{Model, ModelInstance, ModelName, ModelVersion}
 
 class H2OModel extends Model {
     //TODO placeholder model - we should create this model based on some config
-    private val genModel: GenModel = new DeeplearningMojoModel(Array(), Array(), "")
+    private val genModel: GenModel = MojoModel.load("")
     //TODO similarly to evaluator in PMMLModel we can use this wrapper to extract signature and score the model in ModelInstance
     val modelWrapper: EasyPredictModelWrapper = new EasyPredictModelWrapper(genModel)
 
