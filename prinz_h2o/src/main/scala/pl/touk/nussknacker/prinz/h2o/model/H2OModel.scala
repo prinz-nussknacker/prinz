c08d9c9fcdc321527bb341f16137e7ff0f2bf05a
Jan Kukowski <57066180+xvk9@users.noreply.github.com>
2021-04-14 23:30:14 +0200

    H2O ModelSignature (#144)
    * H2OSignatureProvider
    
    * Errox fix
    
    * Style fix

--- prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
+++ prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
@@ -1,3 +1,18 @@
 package pl.touk.nussknacker.prinz.h2o.model
 
-class H2OModel
+import hex.genmodel.{GenModel, MojoModel}
+import hex.genmodel.easy.EasyPredictModelWrapper
+import pl.touk.nussknacker.prinz.model.{Model, ModelInstance, ModelName, ModelVersion}
+
+class H2OModel extends Model {
+    //TODO placeholder model - we should create this model based on some config
+    private val genModel: GenModel = MojoModel.load("")
+    //TODO similarly to evaluator in PMMLModel we can use this wrapper to extract signature and score the model in ModelInstance
+    val modelWrapper: EasyPredictModelWrapper = new EasyPredictModelWrapper(genModel)
+
+    override def getName: ModelName = ???
+
+    override def getVersion: ModelVersion = ???
+
+    override def toModelInstance: ModelInstance = ???
+}
