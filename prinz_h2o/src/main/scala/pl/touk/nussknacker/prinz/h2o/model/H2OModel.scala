b9f27d1946f471791300932ea631af6dbdfa8bac
xvk9 <kukowskijan@gmail.com>
2021-04-14 22:04:48 +0200

    H2OSignatureProvider

--- prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
+++ prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
@@ -1,3 +1,19 @@
 package pl.touk.nussknacker.prinz.h2o.model
 
-class H2OModel
+import hex.genmodel.GenModel
+import hex.genmodel.algos.deeplearning.DeeplearningMojoModel
+import hex.genmodel.easy.EasyPredictModelWrapper
+import pl.touk.nussknacker.prinz.model.{Model, ModelInstance, ModelName, ModelVersion}
+
+class H2OModel extends Model {
+    //TODO placeholder model - we should create this model based on some config
+    private val genModel: GenModel = new DeeplearningMojoModel(Array(), Array(), "")
+    //TODO similarly to evaluator in PMMLModel we can use this wrapper to extract signature and score the model in ModelInstance
+    val modelWrapper: EasyPredictModelWrapper = new EasyPredictModelWrapper(genModel)
+
+    override def getName: ModelName = ???
+
+    override def getVersion: ModelVersion = ???
+
+    override def toModelInstance: ModelInstance = ???
+}
---
+++ prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OSignatureProvider.scala
@@ -0,0 +1,26 @@
+package pl.touk.nussknacker.prinz.h2o.model
+
+import hex.genmodel.GenModel
+import pl.touk.nussknacker.engine.api.typed.typing.Typed
+import pl.touk.nussknacker.prinz.model.{Model, ModelSignature, SignatureField, SignatureName, SignatureProvider, SignatureType}
+
+object H2OSignatureProvider extends SignatureProvider {
+
+  override def provideSignature(model: Model): Option[ModelSignature] = model match {
+    case model: H2OModel =>
+      val m = model.modelWrapper.m
+      val inputNames = m.getNames.toList.dropRight(1)
+      val outputName = m.getResponseName
+      val signatureInputs = inputNames.map(name => fromNameDomainToSignatureField(name, m.getDomainValues(name)))
+      val signatureOutputs = List(fromNameDomainToSignatureField(outputName, m.getDomainValues(outputName)))
+      Option(ModelSignature(signatureInputs, signatureOutputs))
+    case _ => throw new IllegalArgumentException("H2OSignatureInterpreter can interpret only H2OModels")
+  }
+
+  private def fromNameDomainToSignatureField(name: String, domain: Array[String]): SignatureField = {
+    SignatureField(
+      SignatureName(name),
+      SignatureType(typingResult = if(domain == null) Typed[Double] else Typed[String])
+    )
+  }
+}
