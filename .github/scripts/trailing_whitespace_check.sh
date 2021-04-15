102840e336fdc949fcc6a2a26928adf41f26413f
avan1235 <macieekprocyk@gmail.com>
2021-04-15 23:53:52 +0200

    trailing space checker not check the docs

--- .github/scripts/trailing_whitespace_check.sh
+++ .github/scripts/trailing_whitespace_check.sh
@@ -5,6 +5,7 @@ set -e
 # Add lines "-o <condition> -prune" to add more exceptions
 files="$(find . \
   -type d \( -path ./.git \) -prune \
+  -type d \( -path ./docs \) -prune \
   -o -type f \( -name "*.jpg" -o -name "*.png" \) -prune \
   -o -type f -exec egrep -l " +$" {} \;)"
 
