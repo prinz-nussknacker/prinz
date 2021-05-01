b92f6224c78eb1096bd6cfd2fd88b6758fd3c905
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-05-02 00:25:49 +0200

    Add sample fraud detection model (#155)

--- dev-environment/h2o/scripts/train.sh
+++ dev-environment/h2o/scripts/train.sh
@@ -3,5 +3,6 @@
 # Wait for H2o server to start and initialize
 sleep 30
 # Run models training and exporting them to MOJO files
-python models/glm-wine/train.py 0.42
+python models/fraud-detection/train.py &&
+python models/glm-wine/train.py 0.42 &&
 python models/glm-wine/train.py 0.84
