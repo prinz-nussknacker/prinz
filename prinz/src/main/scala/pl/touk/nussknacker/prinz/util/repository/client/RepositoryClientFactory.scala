40cf6675f060cff256b5604e4e711dc92d3420f1
Jadw1 <jadw1_1@o2.pl>
2021-05-03 14:58:36 +0200

    order fix

--- prinz/src/main/scala/pl/touk/nussknacker/prinz/util/repository/client/RepositoryClientFactory.scala
+++ prinz/src/main/scala/pl/touk/nussknacker/prinz/util/repository/client/RepositoryClientFactory.scala
@@ -6,15 +6,15 @@ class RepositoryClientFactory(implicit config: RepositoryClientConfig) {
 
   private val path = config.modelsDirectory
 
-  private val client: AbstractRepositoryClient = path.getScheme match {
-      case "http" => new HttpRepositoryClient(path, config.fileExtension, selector)
-      case "file" => new LocalFSRepositoryClient(path, config.fileExtension)
-      case _ => throw new IllegalArgumentException("Unsupported URI scheme in repository location")
-  }
-
   private val selector = config.modelDirectoryHrefSelector match {
     case Some(value) => value
     case None => throw new IllegalStateException("modelDirectoryHrefSelector should be defined when " +
       "using repository based on HTTP")
+  }
+
+  private val client: AbstractRepositoryClient = path.getScheme match {
+    case "http" => new HttpRepositoryClient(path, config.fileExtension, selector)
+    case "file" => new LocalFSRepositoryClient(path, config.fileExtension)
+    case _ => throw new IllegalArgumentException("Unsupported URI scheme in repository location")
   }
 }
