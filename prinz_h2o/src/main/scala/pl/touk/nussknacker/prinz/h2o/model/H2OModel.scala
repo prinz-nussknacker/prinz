e52304f960613f2b69e900e05c56f895237d86b5
avan1235 <macieekprocyk@gmail.com>
2021-05-21 10:30:26 +0200

    clean model creation in h2omodel

--- prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
+++ prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModel.scala
@@ -1,13 +1,10 @@
 package pl.touk.nussknacker.prinz.h2o.model
 
-import hex.genmodel.{GenModel, ModelMojoReader, MojoReaderBackendFactory}
+import hex.genmodel.GenModel
 import hex.genmodel.MojoReaderBackendFactory.CachingStrategy
-import pl.touk.nussknacker.prinz.h2o.model.H2OModel.loadGenModel
 import pl.touk.nussknacker.prinz.h2o.repository.H2OModelPayload
 import pl.touk.nussknacker.prinz.model.SignatureProvider.ProvideSignatureResult
 import pl.touk.nussknacker.prinz.model.{Model, ModelInstance, ModelName, ModelSignatureLocationMetadata, ModelVersion}
-
-import java.net.URL
 
 final class H2OModel(payload: H2OModelPayload, cachingStrategy: CachingStrategy) extends Model {
 
@@ -17,7 +14,7 @@ final class H2OModel(payload: H2OModelPayload, cachingStrategy: CachingStrategy)
     }
 
     override protected val signatureOption: ProvideSignatureResult = {
-        val model = loadGenModel(payload.path, cachingStrategy)
+        val model = H2OModelWrapperExtractor.extractGenModel(payload, cachingStrategy)
         val metadata = H2OModelSignatureLocationMetadata(model)
         H2OSignatureProvider.provideSignature(metadata)
     }
@@ -25,14 +22,6 @@ final class H2OModel(payload: H2OModelPayload, cachingStrategy: CachingStrategy)
     override protected val name: ModelName = H2OModelName(payload.name)
 
     override protected val version: ModelVersion = H2OModelVersion(payload.version)
-}
-
-object H2OModel {
-
-    private def loadGenModel(modelUrl: URL, cachingStrategy: CachingStrategy): GenModel = {
-        val readerBackend = MojoReaderBackendFactory.createReaderBackend(modelUrl, cachingStrategy)
-        ModelMojoReader.readFrom(readerBackend)
-    }
 }
 
 final case class H2OModelName(name: String) extends ModelName(name)
--- prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModelWrapperExtractor.scala
+++ prinz_h2o/src/main/scala/pl/touk/nussknacker/prinz/h2o/model/H2OModelWrapperExtractor.scala
@@ -1,6 +1,6 @@
 package pl.touk.nussknacker.prinz.h2o.model
 
-import hex.genmodel.{ModelMojoReader, MojoReaderBackendFactory}
+import hex.genmodel.{GenModel, ModelMojoReader, MojoReaderBackendFactory}
 import hex.genmodel.MojoReaderBackendFactory.CachingStrategy
 import hex.genmodel.easy.EasyPredictModelWrapper
 import pl.touk.nussknacker.prinz.h2o.repository.H2OModelPayload
@@ -8,8 +8,12 @@ import pl.touk.nussknacker.prinz.h2o.repository.H2OModelPayload
 object H2OModelWrapperExtractor {
 
   def extractModelWrapper(payload: H2OModelPayload, cachingStrategy: CachingStrategy): EasyPredictModelWrapper = {
-    val modelReaderBackend = MojoReaderBackendFactory.createReaderBackend(payload.path, cachingStrategy)
-    val genModel = ModelMojoReader.readFrom(modelReaderBackend)
+    val genModel = extractGenModel(payload, cachingStrategy)
     new EasyPredictModelWrapper(genModel)
+  }
+
+  def extractGenModel(payload: H2OModelPayload, cachingStrategy: CachingStrategy): GenModel = {
+    val modelReaderBackend = MojoReaderBackendFactory.createReaderBackend(payload.path, cachingStrategy)
+    ModelMojoReader.readFrom(modelReaderBackend)
   }
 }
