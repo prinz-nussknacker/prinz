de866525e8ebcd85ffec72afcbca7c16b2295f2d
Krzysztof Antoniak <kantoniak@kantoniak.com>
2021-06-11 15:08:54 +0200

    Simplify output sinks

--- prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/LoggingSink.scala
+++ prinz_sample/src/main/scala/pl/touk/nussknacker/prinz/sample/LoggingSink.scala
@@ -5,14 +5,14 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.streaming.api.functions.sink.SinkFunction.Context
 import pl.touk.nussknacker.engine.flink.api.process.BasicFlinkSink
 
-case class LoggingSink(prefix: String) extends BasicFlinkSink with LazyLogging {
+case class LoggingSink() extends BasicFlinkSink with LazyLogging {
 
   override def testDataOutput: Option[Any => String] =
-    Some(value => s"$prefix - $value")
+    Some(value => s"$value")
 
   override def toFlinkFunction: SinkFunction[Any] = new SinkFunction[Any] with Serializable {
     override def invoke(value: Any, context: Context[_]): Unit = {
-      val loggedMessage = s"$prefix - $value"
+      val loggedMessage = s"$value"
       logger.info(loggedMessage)
     }
   }
